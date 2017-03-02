01f737a1aa2d062f43f008420f5f6b43b5568885
Arkturous <holli.p@gmx.com>
2017-03-02 11:38:07 -0800

    Home.js changed for different jade view target for the taxonomies: start_wizard

--- components/web/controllers/home.js
+++ components/web/controllers/home.js
@@ -9,10 +9,19 @@ exports.index = function (req, res) {
   });
 }
 
+/**
+/*
+/* Changed the taxonomies entry to use start_wizard.jade
+/* rather than home.jade.
+/* This is where the user will select interests, skills and goals to match.
+/*
+/*  res.render(res.locals.brigade.theme.slug + '/views/home', {
+*/
+
 exports.projectList = function (req, res) {
-  res.render(res.locals.brigade.theme.slug + '/views/home', {
-    view: 'home',
-    title: 'Home',
+  res.render(res.locals.brigade.theme.slug + '/views/start_wizard', {
+    view: 'start_wizard',
+    title: 'Interests, Skills, Goals',
     brigade: res.locals.brigade,
     skills: res.locals.projectTaxonomySkills,
     interests: res.locals.projectTaxonomyInterests,
