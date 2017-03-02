c5eee29353db52d2d09666633f5bc1369026a8d2
Arkturous <holli.p@gmx.com>
2017-03-02 13:19:22 -0800

    Change to the matching controller

--- components/web/controllers/matching.js
+++ components/web/controllers/matching.js
@@ -5,6 +5,9 @@ exports.index = function (req, res) {
   res.render(res.locals.brigade.theme.slug + '/views/matching', {
     view: 'matching',
     title: 'Matching Hat',
-    brigade: res.locals.brigade,
+/**
+/* send the matching output object back to matching.jade
+   using "projects"                                     */
+    projects: res.locals.output,
   });
 }
