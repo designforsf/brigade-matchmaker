b78e6f4afcc8cbb5cd3e6915a2636e2798a78460
Arkturous <holli.p@gmx.com>
2017-03-15 18:56:08 -0700

    rough draft for all views

--- components/web/themes/codeforsf/public/js/_checkin.js
+++ components/web/themes/codeforsf/public/js/_checkin.js
@@ -2,17 +2,24 @@
 $(function() {
 //When doc ready set up event handlers
   $( "#checkin" ).submit(function( event ) {
-
+    event.preventDefault;
     if ( localStorage.getItem("checkedIn") === "true" ) {
       window.alert("You are already checked in using " + localStorage.getItem("emailAddr") );
+      $("#ahead").attr("href", "/test/api/projects");
+      $("#ahead").removeProp("disabled")
+      //window.location.href = "/test/api/projects";
     }
     else {
       postCheckin( event ) ;
-      event.preventDefault(); }
+       }
   });
   $( "#resetLocalStore" ).on( "click", function( event ) {
+    window.alert("reset local");
     if ( localStorage.getItem("checkedIn") )
       localStorage.setItem("checkedIn", false);
+      localStorage.setItem("emailAddr", "");
+      localStorage.setItem("firstName", "");
+      localStorage.setItem("lastName", "");
     window.alert("You are now reset to not checked in ")
     });
 })
@@ -21,14 +28,15 @@ var checkedIn = false;
 
 function postCheckin( evt ) {
   //var firstNm = $("#first_name").attr("name");
+  window.alert("checking in now");
   var checkinData = {
     firstName : $("#first_name").val(),
     lastName : $("#last_name").val(),
     emailAddr : $("#email").val()
   };
   $.ajax ( {
-    type : "POST",
-    url : "/login",
+    type : "GET",
+    url : "/test/api/projects",
     data : checkinData,
     success : success(checkinData)
   });
@@ -36,6 +44,7 @@ function postCheckin( evt ) {
 
 function success( checkinData, evt ) {
   checkedIn = "true";
+  $("a#ahead").attr("href", "/test/api/projects").removeProp("disabled");
   //check for local storage and store the user checkin info
   if (typeof(Storage) !== "undefined") {
     localStorage.setItem("checkedIn" , "true"); //stored as string
