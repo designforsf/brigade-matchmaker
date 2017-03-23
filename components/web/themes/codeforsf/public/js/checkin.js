40eda798d919d783634e2ced8e437ddbaf43146f
Jamie Pitts <james.pitts@gmail.com>
2017-03-22 20:27:15 -0700

    Wizard: fixed the login with a client-side call to JSON API. Minor fixes to the layout (refs to removed js and css files).

--- components/web/themes/codeforsf/public/js/checkin.js
+++ components/web/themes/codeforsf/public/js/checkin.js
@@ -15,57 +15,6 @@
     console.log("checkin click");
 
     if ( localStorage.getItem("checkedIn") === "true" ) {
-<<<<<<< HEAD:components/web/themes/codeforsf/public/js/checkin.js
-      console.log("You are already checked in using " + localStorage.getItem("emailAddr") );
-      //$("#ahead").attr("href", "/test/api/projects");
-      //$("#ahead").removeProp("disabled")
-       = "/test/projectlist";
-    }
-    else {
-      postCheckin( event ) ;
-       }
-  });
-  $( "#resetLocalStore" ).on( "click", function( event ) {
-    if ( localStorage.getItem("checkedIn") )
-      localStorage.setItem("checkedIn", false);
-      localStorage.setItem("emailAddr", "");
-      localStorage.setItem("firstName", "");
-      localStorage.setItem("lastName", "");
-    });
-})
-
-var checkedIn = false;
-
-function postCheckin( evt ) {
-  //var firstNm = $("#first_name").attr("name");
-  console.log("checking in now: " + $("#email").val() );
-  var checkinData = {
-    firstName : $("#first_name").val(),
-    lastName : $("#last_name").val(),
-    emailAddr : $("#email").val()
-  };
-  window.location.href = "/test/projectlist";
-  $.ajax ( {
-    type : "GET",
-    url : "/test/projectlist",
-    data : checkinData,
-    success : saveLocal(checkinData)
-  });
-}
-
-function saveLocal( checkinData, evt ) {
-  console.log("saveLocal event is : ", evt)
-  checkedIn = "true";
-  //$("a#ahead").attr("href", "/test/api/projects").removeProp("disabled");
-  //check for local storage and store the user checkin info
-  if (typeof(Storage) !== "undefined") {
-    localStorage.setItem("checkedIn" , "true"); //stored as string
-    localStorage.setItem("emailAddr" , checkinData.emailAddr );
-    localStorage.setItem("firstName" , checkinData.firstName);
-    localStorage.setItem("lastName" , checkinData.lastName);
-} else {
-    console.log("Sorry! No Web Storage support..");
-=======
       window.alert("You are already checked in using " + localStorage.getItem("emailAddr") );
       
       //window.location.href = "/test/api/projects";
@@ -123,20 +72,28 @@ function saveLocal( checkinData, evt ) {
 
   var checkedIn = false;
 
-   Checkin.postCheckin = function postCheckin(  ) {
+  /*
+    post checkin
+  */
+
+  Checkin.postCheckin = function postCheckin(  ) {
     //var firstNm = $("#first_name").attr("name");
     console.log("checking in now");
+    
     var checkinData = {
       firstName : $("#first_name").val(),
       lastName : $("#last_name").val(),
       emailAddr : $("#email").val()
     };
+    
+    /*
     $.ajax ( {
       type : "GET",
       url : "/test/api/projects",
       data : checkinData,
       success : Checkin.success(checkinData)
     });
+    */
   }
 
    Checkin.success = function success( checkinData, evt ) {
@@ -152,9 +109,9 @@ function saveLocal( checkinData, evt ) {
       window.alert("Sorry! No Web Storage support..");
     }
     
+    // forward to the wizard interface
     window.location="/test/projectlist";
     
->>>>>>> 615781e3cac3138afdc2466281b5418caa95f24b:components/web/themes/codeforsf/public/js/_checkin.js
   }
   
   }) (( window.MH=window.MH || {}));
