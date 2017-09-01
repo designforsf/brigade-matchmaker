586bc3554f026d1bb2da4f236d024c3f40a74dbe
Jamie Pitts <james.pitts@gmail.com>
2017-09-01 16:19:25 -0700

    Web: renamed the main template to "project-match".

--- components/web/controllers/home.js
+++ components/web/controllers/home.js
@@ -28,9 +28,8 @@ exports.index = function (req, res) {
 */
 
 exports.projectList = function (req, res) {
-  res.render(res.locals.brigade.theme.slug + '/views/MHwizard', {
-    view: 'start_wizard',
-    title: 'Interests, Skills, Goals',
+  res.render(res.locals.brigade.theme.slug + '/views/project-match', {
+    title: 'Project Match',
     brigade: res.locals.brigade,
     skills: res.locals.projectTaxonomySkills,
     interests: res.locals.projectTaxonomyInterests,
