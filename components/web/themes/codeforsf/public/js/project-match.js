a2105e7f39d1dcd5aa7dbd06b19c42b085d7c487
Jamie Pitts <james.pitts@gmail.com>
2018-01-08 17:09:12 -0800

    Web App: Created a client closure ProjectMatch, integrated the use of requirejs. Improved the use of jade templates with Bootstrap. Improved the navigation and landing intro, enabling the user to close the landing intro. Removed cruft code from the early prototype. Removed cruft code from BrigadeHub.

---
+++ components/web/themes/codeforsf/public/js/project-match.js
@@ -0,0 +1,78 @@
+define(['jquery', 'projlistview','projlistmodel','taxsel', 'taxselmodel'],
+	function(jQuery, ProjectView, ProjectModel, TaxonomySelector, TaxonomyModel){
+
+	// ProjectMatch closure
+	(function (PM) {
+
+		/* 
+			init
+
+			Main client initialization
+			Should be called from HTML
+			
+		*/
+
+		PM.init = function () {
+
+			console.log('ProjectMatch.init()');
+
+	    // project list component
+	    projView = new ProjectView({ skills:[], learnSkills:[], interests:[] });
+
+	    // taxonomy selector component
+	    var taxonomySel = ProjectMatch.TaxonomySelector;
+	    taxonomySel.init({
+	      // callback when the user clicks on the generate match button
+	      generateMatchCb: function () {
+
+	        // retracts the selector so the user can focus on the results
+	        taxonomySel.closeSelector();
+
+	        // search with the current user's taxonomy selection
+	        projView.searchProjects(taxonomySel.getSelection());
+	      }
+	    });
+
+  	} // END init
+
+
+		// UI activate landing
+
+	  PM.uiActivateLanding = function () {
+
+	      // show top nav
+	      //jQuery('#top-nav').hide();
+	      
+	      // hide the landing page intro
+	      jQuery('#landing-intro').slideDown({
+	        duration: 350,
+	        complete:function () {}
+	      });
+
+	  }; // END uiActivateLanding
+
+
+	  // UI activate navigation
+
+	  PM.uiActivateNavigation = function () {
+
+	      // hide the landing page intro
+	      jQuery('#landing-intro').slideUp({
+	        duration: 350,
+	        complete:function () {
+
+	        	// show top nav
+	      		//jQuery('#top-nav').show();
+
+	        }
+	      });
+
+	  }; // END uiActivateNavigation
+
+	}) (( window.ProjectMatch=window.ProjectMatch || {})); // END ProjectMatch closure
+
+
+	// return the object to requirejs
+	return window.ProjectMatch;
+
+});
