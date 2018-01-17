1acdb30d9994cf98677a4472c1c7e80ec4ee0d54
Jamie Pitts <james.pitts@gmail.com>
2018-01-17 00:05:13 -0800

    Web App: fixed a naming bug in taxonomies-for-ui. Documentation: updated the API docu with taxonomies-for-ui.

--- components/web/controllers/api.js
+++ components/web/controllers/api.js
@@ -767,7 +767,7 @@ module.exports = {
               cb(err, {
                   name: 'skills',
                   title: 'Skills',
-                  items: itemsBySection
+                  itemsBySection: itemsBySection
               });
             });
           },
