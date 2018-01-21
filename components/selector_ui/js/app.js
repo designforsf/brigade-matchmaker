345f314fd666c0b7f171ee0902089e923d5be7ff
alschmiedt <schmiedtabby@gmail.com>
2018-01-21 13:50:53 -0800

    Added ability for views to work with multiple on a page

--- components/selector_ui/js/app.js
+++ components/selector_ui/js/app.js
@@ -13,4 +13,16 @@ require(['views/MessagingView'], function(MessagingView){
       'tag-color':'#AA193A',
       'url' : 'test-url'
    });
+   new MessagingView({
+      'name':'Interests',
+      'tag-color':'#3DA1D2',
+      'url' : 'test-url',
+      'el' : '#container2'
+   });
+   new MessagingView({
+      'name':'Learnings',
+      'tag-color':'#123D51',
+      'url' : 'test-url',
+      'el' : '#container3'
+   });
 });
