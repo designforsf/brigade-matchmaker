41c8cb16f20dcab599681a194bd826672c228631
Jamie Pitts <james.pitts@gmail.com>
2018-04-03 16:00:57 -0700

    MinMaximizer: created a component which can house the message panel within the web app. This is not complete.

---
+++ components/minmaximizer/public/js/minmaximizer.js
@@ -0,0 +1,23 @@
+
+(function (PM) {
+
+  ProjectMatch.Minmaximizer = {};
+  var self = ProjectMatch.Minmaximizer;
+
+
+  /*
+    init
+
+    loads click handlers
+    loads minmax floaters into the UI
+
+  */
+
+
+  self.init = function (attr) {
+
+
+  } // END self.init
+
+
+ }) (( window.ProjectMatch=window.ProjectMatch || {}));
