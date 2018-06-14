fbb64346c89ff839b09aabc0749807dc3017e63c
Paul Jickling <pjickling@gmail.com>
2018-06-13 20:56:39 -0700

    Iterated on Project model with a flattened structure

--- components/admin/data/admin.py
+++ components/admin/data/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Test
+from .models import Project
 
 # Register your models here.
-admin.site.register(Test)
+admin.site.register(Project)
--- components/admin/data/models.py
+++ components/admin/data/models.py
@@ -1,91 +1,28 @@
 from django.db import models
 
-# test models to make sure djongo working as intended
+# the taxonomy model will need to appear before the Project model
 
-class SuperAdmin(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    github_name = models.CharField(max_length=200)
+# prototype project model
+
+class Project(models.Model):
+    id = models.CharField(max_length=2000, primary_key=True)# should be the MongoDB id
+
+    # Project List data commented out lines to be implemented in the future
+    name = models.CharField(max_length=2000)                # project name
+    summary = models.CharField(max_length=200)              # should be one or two sentences
+#    skills_needed = models.ForeignKey(Skills, on_delete=models.CASCADE)
+#    learning_opportunities = models.ForeignKey(Skills, on_delete=models.CASCADE)
+#    civic_interests = models.ForeignKey(Civic, on_delete=models.CASCADE)
+
+    # Project Page data
+    image_url = models.CharField(max_length=2000)           # the image url for the project
+    description = models.TextField()                        # the blurb for the app, can be long
+    project_status = models.CharField(max_length=50)       # "active", "prototype", "beta", etc.
+    repository = models.CharField(max_length=2000)          # github repository
+    website = models.CharField(max_length=2000)            # if there is an app or site the url can go here
+#    team_lead = models.ForeignKey(User, on_delete=models.CASCADE)
+    slack_channel = models.CharField(max_length=200)        # the slack channel for the 
 
     def __str__(self):
-        return self.first_name
-        return self.last_name
-        return self.email
-        return self.github_name
-
-class Test(models.Model):
-    string = models.CharField(max_length=200)
-    boolean = models.BooleanField()
-
-# prototype projects model
-
-# some things:
-# team has anonymous objects that have avatars and usernames
-# matchingconfig has goals needed(array), skillsOffered(array), skillsNeeded(array), interests(array)
-# needs(array)
-
-
-
-# some blank things:
-# contact(array)
-# matchingDescr has link, repository, progress(array), tasks(array), contactThumbnailUrl, contactRole, contactSlack, contactEmail, summary, and thumbnailUrl
-
-class Team(models.Model):
-    avatar = models.CharField(max_length=2000)
-    username = models.CharField(max_length=2000)
-
-class Progress(models.Model):
-    progress = models.CharField(max_length=2000)
-
-class Tasks(models.Model):
-    tasks = models.CharField(max_length=2000)
-
-class GoalsNeeded(models.Model):
-    goalsNeeded = models.CharField(max_length=2000)
-
-class SkillsOffered(models.Model):
-    skillsOffered = models.CharField(max_length=2000)
-
-class SkillsNeeded(models.Model):
-    skillsNeeded = models.CharField(max_length=2000)
-
-class Interests(models.Model):
-    interests = models.CharField(max_length=2000)
-
-class Needs(models.Model):
-    needs = models.CharField(max_length=2000)
-
-class ThumbNailUrl(models.Model):
-    thumbNailUrl = models.CharField(max_length=2000)
-
-class MatchingDescr(models.Model):
-    link = models.CharField(max_length=2000)
-    repository = models.CharField(max_length=2000)
-    progress = models.ManyToManyField(Progress, max_length=2000)
-    tasks = models.ManyToManyField(Tasks, max_length=2000)
-    contactThumbNailUrl = models.CharField(max_length=2000)
-    contactRole = models.CharField(max_length=2000)
-    contactSlack = models.CharField(max_length=2000)
-    contactEmail = models.CharField(max_length=2000)
-    contactName = models.CharField(max_length=2000)
-    summary = models.CharField(max_length=2000)
-    thumbnailUrl = models.CharField(max_length=2000)
-
-class MatchingConfig(models.Model):
-    goalsNeeded = models.ManyToManyField(GoalsNeeded, max_length=2000)
-    skillsOffered = models.ManyToManyField(SkillsOffered, max_length=2000)
-    skillsNeeded = models.ManyToManyField(SkillsNeeded, max_length=2000)
-    interests = models.ManyToManyField(Interests, max_length=2000)
-    needs = models.ManyToManyField(Needs, max_length=2000)
-    thumbNailUrl = models.ForeignKey(ThumbNailUrl, on_delete=models.CASCADE, max_length=2000)
-
-class Projects(models.Model):
-    id = models.CharField(max_length=2000, primary_key=True)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, max_length=2000)
-    repository = models.CharField(max_length=2000)
-    homepage = models.CharField(max_length=2000)
-    description = models.TextField()
-    name = models.CharField(max_length=2000)
-    matchingDescr = models.ForeignKey(MatchingDescr, on_delete=models.CASCADE, max_length=2000)
-    matchingConfig = models.ForeignKey(MatchingConfig, on_delete=models.CASCADE, max_length=2000)
+        return self.name
+        return self.summary
