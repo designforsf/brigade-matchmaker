845748ff543583bde235cb8a7e6006d96a3fca9a
Paul Jickling <pjickling@gmail.com>
2018-08-06 18:07:43 -0700

    Added admin modelforms

--- components/admin/api/admin.py
+++ components/admin/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import SkillsTaxonomy, InterestsTaxonomy, GoalsTaxonomy, Project, ProjectLead
 # Register your models here.
+admin.site.register(SkillsTaxonomy)
+admin.site.register(InterestsTaxonomy)
+admin.site.register(GoalsTaxonomy)
+admin.site.register(ProjectLead)
+admin.site.register(Project)
