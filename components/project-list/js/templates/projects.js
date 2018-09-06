ed251241e560d22094a4d14a0db1a7478c8e96f2
Jamie Pitts <jpitts@users.noreply.github.com>
2018-09-05 23:36:19 -0700

    Project List and Selector UI fixes.

--- components/project-list/js/templates/projects.js
+++ components/project-list/js/templates/projects.js
@@ -4,13 +4,13 @@ this["ProjectList"]["templates"] = this["ProjectList"]["templates"] || {};
 this["ProjectList"]["templates"]["projects"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data,blockParams) {
     var stack1, helper, alias1=container.lambda, alias2=container.escapeExpression, alias3=depth0 != null ? depth0 : (container.nullContext || {}), alias4=helpers.helperMissing, alias5="function";
 
-  return "\n    <div class=\"project\">\n\n        <div class=\"flex-container\">\n\n            <div class=\"left\">\n                <div class=\"project-title\">"
+  return "\n    <div class=\"project\">\n\n        <div class=\"flex-container\">\n\n            <div class=\"project-left\">\n                <div class=\"project-title\">"
     + alias2(alias1(((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1.name : stack1), depth0))
     + "</div>\n                <div class=\"description\">"
     + alias2(alias1(((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1.description : stack1), depth0))
     + "</div>\n                <div id=\"contact-btn"
     + alias2(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias4),(typeof helper === alias5 ? helper.call(alias3,{"name":"index","hash":{},"data":data,"blockParams":blockParams}) : helper)))
-    + "\" class=\"contact-btn\">\n                    Contact Project Lead\n                </div>\n            </div>\n            <div class=\"divider\"></div>\n            <div class=\"right\">\n                <div class=\"project-info\">Project Needs & Interests</div>\n                <div id=\"skills"
+    + "\" class=\"contact-btn\">\n                    Contact Project Lead\n                </div>\n            </div>\n            <div class=\"divider\"></div>\n            <div class=\"project-right\">\n                <div class=\"project-info\">Project Needs & Interests</div>\n                <div id=\"skills"
     + alias2(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias4),(typeof helper === alias5 ? helper.call(alias3,{"name":"index","hash":{},"data":data,"blockParams":blockParams}) : helper)))
     + "\" class=\"skills\">\n                    <div class=\"tag-title\">Skills Needed:</div>\n                    <div class=\"tag-container\">\n"
     + ((stack1 = helpers.each.call(alias3,((stack1 = ((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1.matchingConfig : stack1)) != null ? stack1.skillsNeeded : stack1),{"name":"each","hash":{},"fn":container.program(2, data, 1, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
