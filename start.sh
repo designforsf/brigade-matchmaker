fc8912a341b6c0e7a2cd43d3dd1813f549b55830
Tyler Sellmayer <tyler.sellmayer@gmail.com>
2018-10-17 21:30:18 -0700

    Fix missing mongoose dependency.

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
-cd /app/components/web
+cd /app
 export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use v6.14.4
 npm rebuild node-sass --force
-node app.js
+
+# Load the required seed data.
+( cd components/api ; NODE_ENV=development node scripts/load-seed-data.js )
+
+# Run the system in development mode.
+( cd components/api ; NODE_ENV=development node app.js ) &
+( cd components/main_website ; NODE_ENV=development node app.js ) &
+( cd components/messaging ; NODE_ENV=development node app.js )
