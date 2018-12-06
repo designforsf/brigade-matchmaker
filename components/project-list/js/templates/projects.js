98d82ed6efbe2f1ca619ab8820568a0836cc8baa
Paul Jickling <pjickling@gmail.com>
2018-12-05 21:10:06 -0800

    removed contact project lead button

--- components/project-list/js/templates/projects.js
+++ components/project-list/js/templates/projects.js
@@ -8,9 +8,9 @@ this["ProjectList"]["templates"]["projects"] = Handlebars.template({"1":function
     + alias2(alias1(((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1.name : stack1), depth0))
     + "</div>\n                <div class=\"description\">"
     + alias2(alias1(((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1.description : stack1), depth0))
-    + "</div>\n                <div id=\"contact-btn"
+    + "</div>\n                <!--<div id=\"contact-btn"
     + alias2(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias4),(typeof helper === alias5 ? helper.call(alias3,{"name":"index","hash":{},"data":data,"blockParams":blockParams}) : helper)))
-    + "\" class=\"contact-btn\">\n                    Contact Project Lead\n                </div>\n            </div>\n            <div class=\"divider\"></div>\n            <div class=\"project-right\">\n                <div class=\"project-info\">Project Needs & Interests</div>\n                <div id=\"skills"
+    + "\" class=\"contact-btn\">\n                    Contact Project Lead\n                </div> -->\n            </div>\n            <div class=\"divider\"></div>\n            <div class=\"project-right\">\n                <div class=\"project-info\">Project Needs & Interests</div>\n                <div id=\"skills"
     + alias2(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias4),(typeof helper === alias5 ? helper.call(alias3,{"name":"index","hash":{},"data":data,"blockParams":blockParams}) : helper)))
     + "\" class=\"skills\">\n                    <div class=\"tag-title\">Skills Needed:</div>\n                    <div class=\"tag-container\">\n"
     + ((stack1 = helpers.each.call(alias3,((stack1 = ((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1.newMatchingConfig : stack1)) != null ? stack1.skillsNeeded : stack1),{"name":"each","hash":{},"fn":container.program(2, data, 1, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
@@ -22,13 +22,13 @@ this["ProjectList"]["templates"]["projects"] = Handlebars.template({"1":function
     + alias2(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias4),(typeof helper === alias5 ? helper.call(alias3,{"name":"index","hash":{},"data":data,"blockParams":blockParams}) : helper)))
     + "\" class=\"interests\">\n                    <div class=\"tag-title\">Civic Interests:</div>\n                    <div class=\"tag-container\">\n"
     + ((stack1 = helpers.each.call(alias3,((stack1 = ((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1.newMatchingConfig : stack1)) != null ? stack1.interests : stack1),{"name":"each","hash":{},"fn":container.program(6, data, 1, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
-    + "                    </div>\n                </div>\n            </div>\n        </div>\n\n    \n        <div class=\"details\" id=\"details-"
+    + "                    </div>\n                </div>\n            </div>\n        </div>\n\n\n        <div class=\"details\" id=\"details-"
     + alias2(alias1(((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1._id : stack1), depth0))
     + "\">\n\n            <div class=\"details-flex\">\n                <div class=\"left\">\n                    Pending Tasks\n                    <div class=\"details-container\">\n"
     + ((stack1 = helpers["if"].call(alias3,((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1.todoItems : stack1),{"name":"if","hash":{},"fn":container.program(8, data, 0, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
     + "                    </div>\n                </div>\n                <div class=\"divider\"></div>\n                <div class=\"center\">\n                    Progress Made\n                    <div class=\"details-container\">\n"
     + ((stack1 = helpers["if"].call(alias3,((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1.progressItems : stack1),{"name":"if","hash":{},"fn":container.program(11, data, 0, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
-    + "                    </div>\n                </div>\n                <div class=\"divider\"></div>  \n                <div class=\"right\">\n                    Additional Info\n                    <div class=\"details-container\">\n                        \n                    </div>\n                </div>\n            </div>\n\n        </div>\n\n        <div class=\"details-footer\" id=\"details-button-"
+    + "                    </div>\n                </div>\n                <div class=\"divider\"></div>\n                <div class=\"right\">\n                    Additional Info\n                    <div class=\"details-container\">\n\n                    </div>\n                </div>\n            </div>\n\n        </div>\n\n        <div class=\"details-footer\" id=\"details-button-"
     + alias2(alias1(((stack1 = ((stack1 = blockParams[0][0]) != null ? stack1.attributes : stack1)) != null ? stack1._id : stack1), depth0))
     + "\">\n            Show project details\n        </div>\n\n    </div>\n\n";
 },"2":function(container,depth0,helpers,partials,data,blockParams) {
@@ -80,5 +80,5 @@ this["ProjectList"]["templates"]["projects"] = Handlebars.template({"1":function
 
   return "<div class=\"container\">\n    <div class=\"project-header\">Project List</div>\n\n"
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.projects : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 1, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
-    + "    <!--\n    <button class=\"changeList\">Change List</button>\n    //-->\n\n</div>";
+    + "    <!--\n    <button class=\"changeList\">Change List</button>\n    //-->\n\n</div>\n";
 },"useData":true,"useBlockParams":true});
