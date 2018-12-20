e87766550bec1713ace80be2462cbc5b88144632
Tyler Sellmayer <tyler.sellmayer@gmail.com>
2018-12-19 19:40:08 -0800

    A Docker stack that almost works.

--- start-in-docker.sh
+++ start-in-docker.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 cd /app
-export NVM_DIR="/root/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm use v6.14.4
 npm rebuild node-sass --force
+export NODE_ENV=development
 
 # Load the required seed data.
-( cd components/api ; NODE_ENV=development node scripts/load-seed-data.js )
+( cd components/api ; node scripts/load-seed-data.js )
 
 # Run the system in development mode.
 echo "Starting api component in start-in-docker.sh."
-( cd components/api ; NODE_ENV=development node app.js ) &
+( cd components/api ; node app.js ) &
 echo "Starting messaging component in start-in-docker.sh."
-( cd components/messaging ; NODE_ENV=development node app.js ) &
+( cd components/messaging ; node app.js ) &
 echo "Starting main_website component in start-in-docker.sh."
-( cd components/main_website ; NODE_ENV=development node app.js )
+( cd components/main_website ; node app.js )
 echo "Started app!"
