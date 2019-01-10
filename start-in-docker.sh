4d26e582574f7a5fb4fdb6912436b205d07ba370
Tyler Sellmayer <tyler.sellmayer@gmail.com>
2019-01-09 20:14:24 -0800

    Fix load-seed-data.js so it exits when done.

--- start-in-docker.sh
+++ start-in-docker.sh
@@ -1,4 +1,3 @@
-# TODO(tsell): Remove the disgusting sleeps and fix load-seed-data.js so it doesn't hang.
 cd /app
 export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -6,13 +5,13 @@ nvm use v6.14.4
 npm rebuild node-sass --force
 
 # Load the required seed data.
-( cd components/api ; NODE_ENV=development node scripts/load-seed-data.js ; sleep 10) &
+( cd components/api ; NODE_ENV=development node scripts/load-seed-data.js )
 
 # Run the system in development mode.
 echo "Starting api component in start-in-docker.sh."
-( cd components/api ; NODE_ENV=development node app.js ; sleep 10) &
+( cd components/api ; NODE_ENV=development node app.js ) &
 echo "Starting messaging component in start-in-docker.sh."
-( cd components/messaging ; NODE_ENV=development node app.js ; sleep 10) &
+( cd components/messaging ; NODE_ENV=development node app.js ) &
 echo "Starting main_website component in start-in-docker.sh."
-( cd components/main_website ; NODE_ENV=development node app.js ; sleep 10)
+( cd components/main_website ; NODE_ENV=development node app.js )
 echo "Started app!"
