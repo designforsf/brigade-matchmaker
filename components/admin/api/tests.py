a2099833a38a787851d3e1d4f06c9c4775b9a90e
Paul Jickling <pjickling@gmail.com>
2019-01-29 09:44:21 -0800

    Converted tabs to spaces

--- components/admin/api/tests.py
+++ components/admin/api/tests.py
@@ -3,10 +3,10 @@ from .models import Skill, Interest, Goal, Project
 
 class ProjectTest(TestCase):
 
-	@classmethod
-	def set_up_test_data(cls):
-		Project.objects.create(title="first project title")
-		Project.objects.create(summary="project summary here")
+        @classmethod
+        def set_up_test_data(cls):
+                Project.objects.create(title="first project title")
+                Project.objects.create(summary="project summary here")
 
-	#def test_name_content(self):
+        #def test_name_content(self):
 
--- components/admin/api/views.py
+++ components/admin/api/views.py
@@ -15,47 +15,47 @@ def index(request):
     return render(request, 'index.html',)
 
 def create_project(request):
-	return render(request, 'create_project/index.html',)
+        return render(request, 'create_project/index.html',)
 
 class SkillList(generics.ListCreateAPIView):
-	queryset = Skill.objects.all()
-	serializer_class = SkillSerializer
-	permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+        queryset = Skill.objects.all()
+        serializer_class = SkillSerializer
+        permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 class SkillDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Skill.objects.all()
-	serializer_class = SkillSerializer
-	permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+        queryset = Skill.objects.all()
+        serializer_class = SkillSerializer
+        permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 class InterestList(generics.ListCreateAPIView):
-	queryset = Interest.objects.all()
-	serializer_class = InterestSerializer
-	permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+        queryset = Interest.objects.all()
+        serializer_class = InterestSerializer
+        permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 class InterestDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Interest.objects.all()
-	serializer_class = InterestSerializer
-	permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+        queryset = Interest.objects.all()
+        serializer_class = InterestSerializer
+        permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 class GoalList(generics.ListCreateAPIView):
-	queryset = Goal.objects.all()
-	serializer_class = GoalSerializer
-	permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)	
+        queryset = Goal.objects.all()
+        serializer_class = GoalSerializer
+        permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 class GoalDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Goal.objects.all()
-	serializer_class = GoalSerializer
-	permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+        queryset = Goal.objects.all()
+        serializer_class = GoalSerializer
+        permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 class ProjectList(generics.ListCreateAPIView):
-	queryset = Project.objects.all()
-	serializer_class = ProjectSerializer
-	permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+        queryset = Project.objects.all()
+        serializer_class = ProjectSerializer
+        permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
-	def perform_create(self, serializer):
-		serializer.save(owner=self.request.user)
+        def perform_create(self, serializer):
+                serializer.save(owner=self.request.user)
 
 class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Project.objects.all()
-	serializer_class = ProjectSerializer
-	permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+        queryset = Project.objects.all()
+        serializer_class = ProjectSerializer
+        permissions_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
