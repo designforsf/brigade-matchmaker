9902fcfe49bb396d3cbc76b1978e2b4ad4d533a8
Paul Jickling <pjickling@gmail.com>
2019-03-27 21:09:30 -0700

    Initial Project Component update

--- components/react/src/Project.js
+++ components/react/src/Project.js
@@ -2,11 +2,28 @@ import React from 'react';
 
 class Project extends React.Component {
   render() {
-    let { attributes: { description, name }  } = this.props;
+    let { title } = this.props;
     return (
-      <div class="well">
-        <h5>{name}</h5>
-        {description}
+      <div className="project" style={{border: "1px solid gray", padding: 16, margin: 16}}>
+      <div className="projectCols" style={{border: "1px solid gray", display: "grid", gridGap: 10, gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))", padding: 16}}>
+        <div className="projectCol1" style={{borderRight: "1px solid gray"}}>
+          <h5>{title}</h5>
+          <p>summary</p>
+          <button>Contact Project Lead</button>
+        </div>
+        <div className="projectCol2">
+          <h5>Project Needs & Interests</h5>
+          <p><strong>Skills Needed:</strong></p>
+          <p><strong>Learning Opportunities:</strong></p>
+          <p><strong>Civic Interests:</strong></p>
+        </div>
+      </div>
+        <details>
+          <summary>Show project details</summary>
+            <p>Pending Tasks</p>
+            <p>Progress Made</p>
+            <p>Additional Info</p>
+        </details>
       </div>
     );
   }
