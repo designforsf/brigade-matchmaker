957fa9a7d315d027305b84ca259182b936684333
Jamie Pitts <jpitts@users.noreply.github.com>
2019-04-22 16:42:50 -0700

    API: put some commonly used testing things into a TestHelper module.

--- components/api/test/api.http-200.test.js
+++ components/api/test/api.http-200.test.js
@@ -1,24 +1,9 @@
-const request = require('supertest');
-const express = require('express');
-const app = express();
 
-// set up the environment-based config
-const Config = require('../../common/lib/ConfigFile.js');
-const config = (new Config({ env: global.process.env.NODE_ENV })).config;
-
-// configurations
-app.locals.config = config;
-app.locals.brigade = 'codeforsf';
-app.set('port', config.api.port);
-
-// routes
-var RouterCfg = require('../express_router');
-routerCfg = new RouterCfg({ config: config, expressApp: app })
-app.use('/', routerCfg.router);
+const testHelper = new ( require('../test-helper.js') );
 
 describe('Test the root path', () => {
     test('It should response the GET method', (done) => {
-        request(app).get('/').then((response) => {
+        testHelper.req(testHelper.app).get('/').then((response) => {
             expect(response.statusCode).toBe(200);
             done();
         });
