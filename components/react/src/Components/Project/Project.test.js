8df5fb0ca02d219ef4a9854db55028db1288120e
Paul Jickling <pjickling@gmail.com>
2019-04-24 20:29:32 -0700

    Added (failing) Project component test

---
+++ components/react/src/Components/Project/Project.test.js
@@ -0,0 +1,11 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import Project from '.';
+
+describe('Project', () => {
+  let project;
+  beforeEach(() => project = shallow(<Project />));
+  it('matches the snapshot', () => {
+    expect(project).toMatchSnapshot();
+  });
+})
