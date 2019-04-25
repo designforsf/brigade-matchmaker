b695a9c52c15c29b377fb42ecdbb332812a153d2
Jamie Pitts <jpitts@users.noreply.github.com>
2019-04-24 20:34:58 -0700

    Admin Ember: re-animated the EmberJS-based admin client. This uses the API.

---
+++ components/admin-ember/app/router.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('projects', function() {
+    this.route('new');
+    this.route('list');
+    this.route('edit', { path: '/:id' });
+  });
+});
+
+export default Router;
