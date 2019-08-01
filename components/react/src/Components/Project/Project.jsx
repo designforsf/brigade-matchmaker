74dbe16c44995dcbfb0650f749944c9d54ca1d27
Paul Jickling <pjickling@gmail.com>
2019-07-31 20:31:51 -0700

    Removed code for styling tags that existed in wrong component. Various stylizations

--- components/react/src/Components/Project/Project.jsx
+++ components/react/src/Components/Project/Project.jsx
@@ -4,29 +4,6 @@ import Tag from '../Tag';
 class Project extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: '',
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-    this.renderTag = this.renderTag.bind(this);
-  }
-
-  handleClick(e) {
-    const clickedName = e.target.textContent;
-    this.setState({
-      name: clickedName,
-    });
-  }
-
-  renderTag(skill) {
-    const defaultBorder = '1px solid #AA193A';
-    const clickedBorder = '1px solid lightgreen';
-
-    if (skill === this.state.name) {
-      return <Tag name={skill} border={clickedBorder} onClick={this.handleClick} />;
-    }
-    return <Tag name={skill} border={defaultBorder} onClick={this.handleClick} />;
   }
 
   render() {
@@ -43,24 +20,24 @@ class Project extends React.Component {
             <h5 className="projectTitle">{name}</h5>
             <p>{description}</p>
             <p className="contact"><strong>Contact Project</strong></p>
-            <p><button type="button">{slackChannel}</button></p>
+            <p><button className="contact-button" type="button">{slackChannel}</button></p>
           </div>
           <div className="projectCol2">
             <h5 className="skillsHeader">Project Needs & Interests</h5>
 
             <p className="skillsCategory"><strong>Skills Needed:</strong></p>
             <div className="tagContainer">
-              { skillsNeeded.map(skill => this.renderTag(skill)) }
+              { skillsNeeded.map(skill => <Tag name={skill} border="1px solid #AA193A" />) }
             </div>
 
             <p className="skillsCategory"><strong>Learning Opportunities:</strong></p>
             <div className="tagContainer">
-              { skillsOffered.map(skill => this.renderTag(skill)) }
+              { skillsOffered.map(skill => <Tag name={skill} border="1px solid #AA193A" />) }
             </div>
 
             <p className="skillsCategory"><strong>Civic Interests:</strong></p>
             <div className="tagContainer">
-              { interests.map(interest => this.renderTag(interest)) }
+              { interests.map(interest => <Tag name={interest} border="1px solid #AA193A" />) }
             </div>
 
           </div>
