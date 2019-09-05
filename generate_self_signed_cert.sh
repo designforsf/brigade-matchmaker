8394be977929e6d5046a8e456a459b2e460a2583
Tyler Sellmayer <tyler.sellmayer@gmail.com>
2019-09-04 21:21:39 -0700

    Generate dhparams in generate_self_signed_cert.sh.

--- generate_self_signed_cert.sh
+++ generate_self_signed_cert.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+# Generate HTTPS certificates.
 openssl req -x509 \
   -out ./localhost_keys/localhost.crt -keyout ./localhost_keys/localhost.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=localhost' -extensions EXT -config <( \
   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+
+# Generate DH params.
+openssl dhparam -out ./dhparam/dhparam-2048.pem 2048
