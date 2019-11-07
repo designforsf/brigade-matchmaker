f62b1adc306d39d289f0c67abb191f429a6a8ede
Jamie Pitts <jpitts@users.noreply.github.com>
2019-11-07 01:21:14 -0800

    Minor improvements to screen-related scripts in devel/v1.

--- devel/v1/stop-screens.sh
+++ devel/v1/stop-screens.sh
@@ -12,6 +12,13 @@ DIR=`pwd`
 
 echo "Stop developing project-match-v1!"
 
+screen -S $SCREENID -X -p2 stuff $'\003'
+screen -S $SCREENID -X -p3 stuff $'\003'
+screen -S $SCREENID -X -p4 stuff $'\003'
+screen -S $SCREENID -X -p5 stuff $'\003'
+
+sleep 1
+
 # stop screen
 echo "Stop the screen session"
 CMD="screen -X -S $SCREENID quit"
