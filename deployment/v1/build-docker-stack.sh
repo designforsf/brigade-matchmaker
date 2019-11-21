725afb14190152ca86d333d630373b449a23c73b
Tyler Sellmayer <tyler.sellmayer@gmail.com>
2019-11-20 21:13:24 -0800

    Add pwd log message.

--- deployment/v1/build-docker-stack.sh
+++ deployment/v1/build-docker-stack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/../../
+echo "Building in $(pwd)"
 docker build -t sfbrigade/project-match:app -f ./deployment/v1/Dockerfile .
