f73e668f153f9ab2cbc605f26fcf25b8701ee560
Paul Jickling <pjickling@gmail.com>
2019-11-20 20:39:54 -0800

    Fixed multitude of UI issues

--- components/react/src/Components/ProjectList/ProjectList.js
+++ components/react/src/Components/ProjectList/ProjectList.js
@@ -6,7 +6,6 @@ const ProjectList = ({ projects, matchScores }) => {
   const sortBy = project => [-(matchScores[project.id] || 0), project.name];
   return (
     <div className="container well">
-      <h1>Project List</h1>
       {_.sortBy(projects, sortBy).map((project, key) =>
         <Project key={key} {...project} />
       )}
