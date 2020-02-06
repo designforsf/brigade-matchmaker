a35d2670d32d347510769a23613113635de1d856
Ricardo Piro-Rael <ricardo@hint.com>
2020-02-05 19:50:37 -0800

    Add react router

--- components/react/src/index.js
+++ components/react/src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/main.scss';
+import './styles/main.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
